adbda7bdfb3793398ae16473b85121b5615a8021
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-03-03 10:41:13 -0700

    disable slidemovies

--- src/providers/sources/slidemovies.ts
+++ src/providers/sources/slidemovies.ts
@@ -70,7 +70,7 @@ export const slidemoviesScraper = makeSourcerer({
   id: 'slidemovies',
   name: 'SlideMovies',
   rank: 135,
-  disabled: false,
+  disabled: true,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
   scrapeShow: comboScraper,
