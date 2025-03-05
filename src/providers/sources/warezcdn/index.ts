66bf19240b46e4fde4b214aca41a647e859ac4be
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-03-04 21:43:57 -0700

    disable warezcdn

--- src/providers/sources/warezcdn/index.ts
+++ src/providers/sources/warezcdn/index.ts
@@ -47,6 +47,7 @@ async function getEmbeds(id: string, servers: string, ctx: ScrapeContext): Promi
 export const warezcdnScraper = makeSourcerer({
   id: 'warezcdn',
   name: 'WarezCDN',
+  disabled: true,
   rank: 115,
   flags: [],
   scrapeMovie: async (ctx) => {
