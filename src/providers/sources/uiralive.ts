07bdca0eb96459348db0e1c998039f45f61cde1f
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-03-16 15:41:23 -0700

    disable uiralive

--- src/providers/sources/uiralive.ts
+++ src/providers/sources/uiralive.ts
@@ -56,7 +56,7 @@ export const uiraliveScraper = makeSourcerer({
   id: 'uiralive',
   name: 'uira.live',
   rank: 250,
-  disabled: false,
+  disabled: true,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
   scrapeShow: comboScraper,
