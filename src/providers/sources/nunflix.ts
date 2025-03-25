b95e9a6be35f05415eb2bff464b0d6478210e88b
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-03-24 22:35:38 -0600

    Update nunflix.ts

--- src/providers/sources/nunflix.ts
+++ src/providers/sources/nunflix.ts
@@ -33,11 +33,22 @@ interface StreamData {
   source: string;
 }
 
+const getUserToken = (): string | null => {
+  try {
+    return typeof window !== 'undefined' ? window.localStorage.getItem('febbox_ui_token') : null;
+  } catch (e) {
+    console.warn('Unable to access localStorage:', e);
+    return null;
+  }
+};
+
 async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promise<SourcererOutput> {
+  const userToken = getUserToken();
+
   const apiUrl =
     ctx.media.type === 'movie'
-      ? `${mamaApiBase}/movie/${ctx.media.tmdbId}`
-      : `${mamaApiBase}/tv/${ctx.media.tmdbId}?season=${ctx.media.season.number}&episode=${ctx.media.episode.number}`;
+      ? `${mamaApiBase}/movie/${ctx.media.tmdbId}&token=${userToken}`
+      : `${mamaApiBase}/tv/${ctx.media.tmdbId}?season=${ctx.media.season.number}&episode=${ctx.media.episode.number}&token=${userToken}`;
 
   const apiRes = await ctx.proxiedFetcher(apiUrl);
 
@@ -128,8 +139,8 @@ async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promis
 export const nunflixScraper = makeSourcerer({
   id: 'nunflix',
   name: 'NFlix',
-  rank: 125,
-  disabled: false,
+  rank: 155,
+  disabled: !!getUserToken(),
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
   scrapeShow: comboScraper,
