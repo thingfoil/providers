36b20d110ce8c81f3210ebd59ba860e51bcfe0b0
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-04-08 21:42:01 -0600

    enable embedsu?

--- src/providers/sources/embedsu.ts
+++ src/providers/sources/embedsu.ts
@@ -65,7 +65,7 @@ export const embedsuScraper = makeSourcerer({
   id: 'embedsu',
   name: 'embed.su',
   rank: 230,
-  disabled: true,
+  disabled: false,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
   scrapeShow: comboScraper,
