4a8f65173835b902d3ff5916793b96e744c2ee74
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-07-07 15:11:03 -0600

    disable autoembed

--- src/providers/sources/autoembed.ts
+++ src/providers/sources/autoembed.ts
@@ -49,7 +49,7 @@ export const autoembedScraper = makeSourcerer({
   id: 'autoembed',
   name: 'Autoembed',
   rank: 110,
-  disabled: false,
+  disabled: true,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
   scrapeShow: comboScraper,
