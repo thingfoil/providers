1eb68dcff4d5002c53b0537728610bd7bcd93540
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-07-07 22:27:15 -0600

    Update vidjoy.ts

--- src/providers/sources/vidjoy.ts
+++ src/providers/sources/vidjoy.ts
@@ -1,16 +1,13 @@
 /* eslint-disable no-console */
 import { flags } from '@/entrypoint/utils/targets';
 import { SourcererOutput, makeSourcerer } from '@/providers/base';
-import { MovieScrapeContext, ShowScrapeContext } from '@/utils/context';
+import { MovieScrapeContext } from '@/utils/context';
 import { NotFoundError } from '@/utils/errors';
 
 const baseUrl = 'https://mia.vidjoy.wtf';
 
-async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promise<SourcererOutput> {
-  const apiUrl =
-    ctx.media.type === 'show'
-      ? `${baseUrl}/tv/${ctx.media.tmdbId}/${ctx.media.season.number}/${ctx.media.episode.number}/index.m3u8`
-      : `${baseUrl}/movies/${ctx.media.tmdbId}/index.m3u8`;
+async function comboScraper(ctx: MovieScrapeContext): Promise<SourcererOutput> {
+  const apiUrl = `${baseUrl}/movies/${ctx.media.tmdbId}/index.m3u8`;
 
   const streamRes = await ctx.proxiedFetcher.full(apiUrl, {
     method: 'GET',
@@ -33,10 +30,7 @@ async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promis
       {
         id: 'primary',
         type: 'hls',
-        playlist:
-          ctx.media.type === 'show'
-            ? `${baseUrl}/tv/${ctx.media.tmdbId}/${ctx.media.season.number}/${ctx.media.episode.number}/index.m3u8`
-            : `${baseUrl}/movies/${ctx.media.tmdbId}/index.m3u8`,
+        playlist: `${baseUrl}/movies/${ctx.media.tmdbId}/index.m3u8`,
         flags: [flags.CORS_ALLOWED],
         captions: [],
       },
@@ -50,5 +44,4 @@ export const vidjoyScraper = makeSourcerer({
   rank: 185,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
-  scrapeShow: comboScraper,
 });
