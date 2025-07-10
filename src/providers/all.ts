e623729a413572a369da6e49723368fbd2e1e02e
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-07-10 10:44:41 -0600

    update zoechip

--- src/providers/all.ts
+++ src/providers/all.ts
@@ -12,6 +12,7 @@ import { mp4hydraScraper } from '@/providers/sources/mp4hydra';
 import { tugaflixScraper } from '@/providers/sources/tugaflix';
 import { vidsrcScraper } from '@/providers/sources/vidsrc';
 import { vidsrcsuScraper } from '@/providers/sources/vidsrcsu';
+import { zoechipScraper } from '@/providers/sources/zoechip';
 
 import {
   autoembedBengaliScraper,
@@ -86,6 +87,7 @@ export function gatherAllSources(): Array<Sourcerer> {
     fsharetvScraper,
     vidsrcsuScraper,
     vidsrcScraper,
+    zoechipScraper,
     mp4hydraScraper,
     embedsuScraper,
     slidemoviesScraper,
