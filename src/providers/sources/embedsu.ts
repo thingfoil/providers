417bb00ccbd63c82877c04c863b76092393c5e18
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-07-10 10:57:41 -0600

    demote embedsu

--- src/providers/sources/embedsu.ts
+++ src/providers/sources/embedsu.ts
@@ -63,7 +63,7 @@ async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promis
 export const embedsuScraper = makeSourcerer({
   id: 'embedsu',
   name: 'embed.su',
-  rank: 170,
+  rank: 165,
   disabled: false,
   flags: [],
   scrapeMovie: comboScraper,
