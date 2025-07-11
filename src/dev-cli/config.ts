c628da6dd138da95a23195807d073c314c1cee88
Chase McCaskill <archsenal.yt@gmail.com>
2025-07-11 13:47:38 -0400

    added pirxcy source

--- src/dev-cli/config.ts
+++ src/dev-cli/config.ts
@@ -1,5 +1,7 @@
 export function getConfig() {
-  let tmdbApiKey = process.env.MOVIE_WEB_TMDB_API_KEY ?? '';
+  let tmdbApiKey =
+    process.env.MOVIE_WEB_TMDB_API_KEY ??
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMjUwMjc3YmZjNjNkNzNiYjY5NmI3MWU2NThjMjUyMSIsIm5iZiI6MTcyNTcyNTQ1OS4wOTksInN1YiI6IjY2ZGM3YjEzNDQyYjExNDQzMmRkMTU5MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.W7jVmiYfA3XQg0eXTdnjer5EkNlJ1F4k4bJyw5zdgVY';
   tmdbApiKey = tmdbApiKey.trim();
 
   if (!tmdbApiKey) {
