f6cda66d1fa612f56e9b50eb5086a678f17149cd
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-07-21 10:15:46 -0600

    disable mp4hydra
    the streams return unknown

--- src/providers/sources/mp4hydra.ts
+++ src/providers/sources/mp4hydra.ts
@@ -71,6 +71,7 @@ export const mp4hydraScraper = makeSourcerer({
   id: 'mp4hydra',
   name: 'Mp4Hydra',
   rank: 4,
+  disabled: true,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
   scrapeShow: comboScraper,
