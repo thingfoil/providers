fa80107610c24527664eb82808e5b2c7f8ca388d
lew <139905304+levrx@users.noreply.github.com>
2025-08-08 11:29:50 +0100

    vidify authorization token

--- src/providers/embeds/vidify.ts
+++ src/providers/embeds/vidify.ts
@@ -9,6 +9,7 @@ const baseUrl = 'api.vidify.top';
 const headers = {
   referer: 'https://api.vidify.top/',
   origin: 'https://player.vidify.top',
+  Authorization: 'Bearer chuitya',
   'User-Agent':
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
 };
