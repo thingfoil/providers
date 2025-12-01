bf249a945171989f86ea440753ae193fb3a6a4a0
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-12-01 15:41:42 -0700

    export labelToLanguageCode in package

--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,4 @@ export { makeStandardFetcher } from '@/fetchers/standardFetch';
 export { makeSimpleProxyFetcher } from '@/fetchers/simpleProxy';
 export { flags, targets } from '@/entrypoint/utils/targets';
 export { setM3U8ProxyUrl, getM3U8ProxyUrl, createM3U8ProxyUrl, updateM3U8ProxyUrl } from '@/utils/proxy';
+export { labelToLanguageCode } from '@/providers/captions';
