5b8fb4f8d35bdec22c3ebf2bc96c8aef0575f7fd
Thingfoil <thingfoil@proton.me>
2026-06-10 22:09:42 +0100

    Remove revalidation

--- src/utils/valid.ts
+++ src/utils/valid.ts
@@ -3,6 +3,9 @@
 import { bombtheirishScraper } from '@/providers/archive/sources/bombtheirish';
 import { streamtapeScraper } from '@/providers/embeds/streamtape';
 import { warezcdnembedMp4Scraper } from '@/providers/embeds/warezcdn/mp4';
+import { cattusScraper } from '@/providers/sources/cattus';
+import { hardpastaScraper } from '@/providers/sources/hardpasta';
+import { witherScraper } from '@/providers/sources/wither';
 import { Stream } from '@/providers/streams';
 import { IndividualEmbedRunnerOptions } from '@/runners/individualRunner';
 import { ProviderRunnerOptions } from '@/runners/runner';
@@ -10,6 +13,9 @@ import { ProviderRunnerOptions } from '@/runners/runner';
 const SKIP_VALIDATION_CHECK_IDS = [
   warezcdnembedMp4Scraper.id,
   streamtapeScraper.id,
+  witherScraper.id,
+  cattusScraper.id,
+  hardpastaScraper.id,
   // deltaScraper.id,
   // alphaScraper.id,
   // novaScraper.id,
